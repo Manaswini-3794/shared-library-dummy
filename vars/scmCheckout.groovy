321cf2b870b702424af1bb41a359116301a3ae15
Dandu Venkata Sai Manaswini <danduvenkatasai.manaswini@nxp.com>
2023-08-31 01:43:45 +0530

    added src, resources directories

--- vars/scmCheckout.groovy
+++ vars/scmCheckout.groovy
@@ -3,19 +3,15 @@ def checkoutRepo(repoMap){
     //def readFromRepo = evaluate(new File('loadRepo.groovy'))
     //def repos = readFromRepo.readRepoList()
 
-    def repos = repoMap.repos
-    def branches = repoMap.branches
-    //def creds = repoMap.creds
-    //def user = repoMap.user
-
-    for(elem in repos){
-        println elem
-        def repourl = "ssh://" + "${USER}" + "@www.collabnet.nxp.com:29418/" + "${it}"
-        checkout([$class: 'GitSCM', branches: [[name: '*/master']],
-        extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: it ]],
+    for(it in repoMap){
+        //println elem.repo
+        //println elem.branch
+        def repourl = "ssh://" + "${USER}" + "@www.collabnet.nxp.com:29418/" + "${it.repo}"
+        checkout([$class: 'GitSCM', branches: [[name: "*/${it.branch}"]],
+        extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: it.repo ]],
         userRemoteConfigs: [[credentialsId: CREDENTIALS, url: repourl]]])
 
-        def folder = new File("${it}@tmp")
+        def folder = new File("${it.repo}@tmp")
         folder.deleteDir()    
     }
    
