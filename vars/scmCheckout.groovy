dcf79b7572e3abbf8f0384b7ff1c3406d08a8ebc
Manaswini-3794 <141128408+Manaswini-3794@users.noreply.github.com>
2023-08-31 14:32:09 +0530

    Update scmCheckout.groovy

--- vars/scmCheckout.groovy
+++ vars/scmCheckout.groovy
@@ -3,7 +3,7 @@ def checkoutRepo(repoMap){
     for(it in repoMap){
         
         def repourl = "ssh://" + "${USER}" + "@www.collabnet.nxp.com:29418/" + "${it.repo}"
-        checkout([$class: 'GitSCM', branches: [[name: "*/${it.branch}"]],
+        checkout([$class: 'GitSCM', branches: [[name: it.branch]],
         extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: it.repo ]],
         userRemoteConfigs: [[credentialsId: CRED_ID, url: repourl]]])
 
